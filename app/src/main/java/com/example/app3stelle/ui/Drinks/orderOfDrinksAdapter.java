008fc7ead9c12a34720514b78a306b943b480809
MARCO GIORGI <marco.giorgi7@studio.unibo.it>
2024-06-27 18:05:42 +0200

    add listener to modify in the db the order's states. Can even remouve a finished order.

--- app/src/main/java/com/example/app3stelle/ui/Drinks/orderOfDrinksAdapter.java
+++ app/src/main/java/com/example/app3stelle/ui/Drinks/orderOfDrinksAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -15,6 +16,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.app3stelle.R;
 import com.example.app3stelle.ui.history.RowElement;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -83,13 +92,17 @@ public class orderOfDrinksAdapter extends RecyclerView.Adapter<orderOfDrinksAdap
             this.adapter = adapter;
         }
 
-        private void showDialog(Context context,String color,String message, int position){
+        private void showDialog(Context context,String color,String message, int position,int state){
             AlertDialog.Builder builder = new AlertDialog.Builder(context);
             builder.setMessage(message)
                     .setPositiveButton("Prosegui", (dialog, id) -> {
                         cardViewBox.setCardBackgroundColor(Color.parseColor(color));
                         if(cardViewBox.getCardBackgroundColor().getDefaultColor() == ContextCompat.getColor(cont, R.color.readyGreen))
                             adapter.printInterface.printOrder(position);
+                        updateOrder(clientNameTextView.getText().toString(),
+                                (textViewDrinkList.getText().toString()),
+                                state
+                                );
                     })
                     .setNegativeButton("Annulla", (dialog, id) -> dialog.dismiss());
             AlertDialog dialog = builder.create();
@@ -100,16 +113,55 @@ public class orderOfDrinksAdapter extends RecyclerView.Adapter<orderOfDrinksAdap
         @Override
         public void onClick(View v) {
             int position = getAdapterPosition();
+            //Toast.makeText(cont, clientNameTextView.getText(), Toast.LENGTH_SHORT).show();
             int cardBackgroundColor = cardViewBox.getCardBackgroundColor().getDefaultColor();
             if (position != RecyclerView.NO_POSITION) {
                 if(cardBackgroundColor == Color.WHITE){
-                    showDialog(cont,"#FFC400", cont.getString(R.string.message_updateOrder),0);
+                    showDialog(cont,"#FFC400", cont.getString(R.string.message_updateOrder),0,1);
                 }else if(cardBackgroundColor == ContextCompat.getColor(cont, R.color.yellow)){
-                    showDialog(cont,"#76FF03",cont.getString(R.string.message_completeOrder),position);
+                    showDialog(cont,"#76FF03",cont.getString(R.string.message_completeOrder),position,2);
                 }else if(cardBackgroundColor == ContextCompat.getColor(cont, R.color.readyGreen)){
+                    updateOrder(clientNameTextView.getText().toString(), (textViewDrinkList.getText().toString()), 3);
                     adapter.removeItem(position);
                 }
             }
+        }
+
+        private void updateOrder(String name,String itemsList,int newState){
+            DatabaseReference refOrdini = FirebaseDatabase.getInstance().getReference("Ordini Drink");
+            Query query = refOrdini.orderByChild("clientName").equalTo(name);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for(DataSnapshot data:snapshot.getChildren()){
+                        if(data.child("descrizione").getValue(String.class).equals(itemsList)){
+                            String key = data.getKey();
+                            if(newState==3){
+                                refOrdini.child(key).removeValue();
+                                break;
+                            }
+                            refOrdini.child(key).child("state").setValue(newState).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(cont, "Ordine aggiornato", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(cont, "Errore Aggiornamento", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                        
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
         }
         }
     }
